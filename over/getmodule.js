1a7bcaa4f75cd7f95213b020d3cb7f72167223e9
xiong shaole <xiong.shaole@dena.jp>
2013-05-22 14:50:20 +0800

    nodejs

---
+++ over/getmodule.js
@@ -0,0 +1,8 @@
+//getmodule.js
+
+var myModule = require('./module');
+
+myModule.setName('nodejs');
+
+myModule.sayHello();
+
---
+++ over/getsingleobject.js
@@ -0,0 +1,7 @@
+//getsingleobject.js
+
+var Hello = require('./singleobject').Hello;
+
+hello = new Hello();
+hello.setName('a');
+hello.sayHello();
