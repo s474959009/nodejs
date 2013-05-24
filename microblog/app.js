f63bdd91587f6602f5e2e49b2db1c2a8aeee71b8
xiong shaole <xiong.shaole@dena.jp>
2013-05-24 13:50:19 +0800

    git log encoding

--- microblog/app.js
+++ microblog/app.js
@@ -33,3 +33,5 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+//哈哈啊这里是中文，会乱码吗？
