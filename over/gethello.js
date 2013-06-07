7aaa6ecd2358521c66788910b1c7c3ea5bdf6926
xiong shaole <xiong.shaole@dena.jp>
2013-06-07 15:11:05 +0800

    sds

--- over/gethello.js
+++
@@ -1,7 +0,0 @@
-//gethello.js
-
-var Hello = require('./hello');
-
-hello = new Hello();
-hello.setName('a');
-hello.sayHello();
--- over/getsingleobject.js
+++
@@ -1,7 +0,0 @@
-//getsingleobject.js
-
-var Hello = require('./singleobject').Hello;
-
-hello = new Hello();
-hello.setName('a');
-hello.sayHello();
