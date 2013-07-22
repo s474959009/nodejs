fa582b55809436be167882caa6be334f77a607d8
enuobear <enuobear@gmail.com>
2013-07-22 15:52:33 +0800

    websocket module

---
+++ websocket/server.js
@@ -0,0 +1,24 @@
+var http = require("http"),
+	io   = require("socket.io");
+
+//create http server
+var server = http.createServer(function(request, response){
+	response.writeHead({"Content-Type": "text/html"});
+	response.end("HTML5 WebSocket Demo");
+});
+server.listen(8000, "localhost");
+
+//wrap http server by socket.io
+var socket = io.listen(server);
+socket.on("connection", function(client){
+	console.log("connect");
+
+	client.on("message", function(data){
+		client.send("Hello" + data);
+	});
+
+	client.on("disconnect", function(){
+		console.log("disconnect");
+	});
+});
+
